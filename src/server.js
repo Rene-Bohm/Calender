7ba58e983c73596af004bc09fe4659270b1b82d9
Rene-Bohm <rene.kassbohm@gmail.com>
2021-03-12 05:41:20 +0100

    Frühzeitige version

--- src/server.js
+++ src/server.js
@@ -7,4 +7,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 5000);
+let port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`starte Server on PORT? ${port}`));
